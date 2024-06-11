5e3262be3d4621d5e44b9332d4563b8fd44c8409
Loïc LESCOAT <loic@example.com>
2024-06-10 22:51:10 -0700

    added text box

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
-from flask import Flask, request, send_file, \
-        render_template, redirect, url_for
+from flask import Flask, request, send_file, render_template, redirect, url_for
 from collections import defaultdict
 
 app = Flask(__name__)
 
-DIR = os.environ['STORAGE_DIR']
+TEXT_STORE = "TEXT_STORE"
 
-messages=defaultdict(str)
-messages['upload'] = "Uploaded file"
-messages['delete'] = "Deleted file"
-messages['No file attached'] = 'No file attached'
-messages['No file chosen'] = 'No file chosen'
+DIR = os.environ["STORAGE_DIR"]
+
+messages = defaultdict(str)
+messages["upload"] = "Uploaded file"
+messages["delete"] = "Deleted file"
+messages["No file attached"] = "No file attached"
+messages["No file chosen"] = "No file chosen"
+messages["upload_text"] = "Text updated"
 
 
 def cwd():
@@ -23,35 +25,45 @@ def cwd():
 
 @app.route("/")
 def home():
-    files = sorted(os.listdir(DIR))
-    return render_template("home.html", files=files, message=messages[request.args.get('action')])
+    files = sorted((x for x in os.listdir(DIR) if x != TEXT_STORE))
+    if os.path.exists(os.path.join(DIR, TEXT_STORE)):
+        with open(os.path.join(DIR, TEXT_STORE)) as f:
+            user_text = f.read()
+    else:
+        user_text = ""
+    return render_template(
+        "home.html",
+        files=files,
+        message=messages[request.args.get("action")],
+        user_text=user_text,
+    )
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
     if "file" not in request.files:
-        return redirect(
-                url_for(
-                    'home',
-                    action='No file attached'
-                    )
-                )
+        return redirect(url_for("home", action="No file attached"))
     file = request.files["file"]
     if file.filename == "":
-        return redirect(
-                url_for(
-                    'home',
-                    action='No file chosen'
-                    )
-                )
+        return redirect(url_for("home", action="No file chosen"))
 
     file.save(os.path.join(DIR, file.filename))
+    return redirect(url_for("home", action="upload"))
+
+
+@app.route("/upload_text", methods=["POST"])
+def upload_text():
+    new_text = request.form.get("text")
+
+    with open(os.path.join(DIR, TEXT_STORE), "w") as f:
+        f.write(new_text)
+
     return redirect(
-            url_for(
-                'home',
-                action='upload'
-                )
-            )
+        url_for(
+            "home",
+            action="upload_text",
+        )
+    )
 
 
 @app.route("/download/<filename>")
@@ -61,15 +73,11 @@ def download(filename):
         return "file not found", 404
     return send_file(full_path)
 
+
 @app.route("/delete/<filename>", methods=["POST"])
 def delete(filename):
     full_path = os.path.join(DIR, filename)
     if not os.path.exists(full_path):
         return "file not found", 404
     os.remove(full_path)
-    return redirect(
-            url_for(
-                'home',
-                action='delete'
-                )
-            )
+    return redirect(url_for("home", action="delete"))
